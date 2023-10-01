cd818b53b8d40ebb37476a33df5b6f1a9cd7ac64
Pranathi Jakkula <127174874+PranathiJakkula@users.noreply.github.com>
2023-10-01 17:31:56 +0530

    Add files via upload

---
+++ QuestionAnswer.java
@@ -0,0 +1,27 @@
+package com.example.quiz;
+
+public class QuestionAnswer {
+    public static  String question[] = {
+            "Which of the following is not a Programming language?",
+            "What is done in Android development?",
+            "Which of the following is not a browser?",
+            "Which software is used to develop the android applications?",
+            "The name of your Internship:"
+
+    };
+    public static  String choices[][] = {
+            {"Kotlin", "Java", "Notepad", "Python"},
+            {"To create wed pages", "To create android applications", "To create gaming platforms", "To create videos"},
+            {"Email", "Google", "Firefox", "OperaMini"},
+            {"Eclipse", "jupiter", "VS Code", "Android Studio"},
+            {"Bharat", "Atlassian", "Eduskills", "rubrik"}
+    };
+    public static  String correctAnswers[] = {
+            "Notepad",
+            "To create android applications",
+            "Email",
+            "Android Studio",
+            "Bharat"
+    };
+
+}
